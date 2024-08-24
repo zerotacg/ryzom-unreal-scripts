47be50eb5f91ec523f71ff0ed02c08a73aaa74f9
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-08-24 10:39:16 +0200

    reformat code

--- zone_materials.py
+++ zone_materials.py
@@ -17,24 +17,24 @@ material_count = len(material_lookup)
 print("Materials {}".format(material_count))
 
 
-def changeZoneMaterials(slow_task, asset_data):
-    zone = asset_data.get_asset()
+def change_zone_materials(slow_task: unreal.ScopedSlowTask, asset_data: unreal.AssetData):
+    zone: unreal.StaticMesh = asset_data.get_asset()
     for j in range(len(zone.static_materials)):
         if slow_task.should_cancel():
             break
-        changeZoneMaterial(slow_task, zone, j);
+        change_zone_material(zone, j)
 
 
-def changeZoneMaterial(slow_task, zone, materialIndex):
-    static_material = zone.static_materials[materialIndex]
-    material_slot = static_material.get_editor_property("material_slot_name")
-    if(material_slot in material_lookup):
-        current_material = zone.get_material(materialIndex);
+def change_zone_material(zone: unreal.StaticMesh, material_index: int):
+    static_material = zone.static_materials[material_index]
+    material_slot: unreal.Name = static_material.get_editor_property("material_slot_name")
+    if material_slot in material_lookup:
+        current_material = zone.get_material(material_index)
         current_material_name = ""
-        if (current_material):
+        if current_material:
             current_material_name = current_material.get_name()
-        if (current_material_name != material_slot):
-            zone.set_material(materialIndex, material_lookup[material_slot].get_asset())
+        if current_material_name != material_slot:
+            zone.set_material(material_index, material_lookup[material_slot].get_asset())
         else:
             print("Material already set {}".format(current_material_name))
     else:
@@ -48,4 +48,4 @@ with unreal.ScopedSlowTask(asset_count, text_label.format("..")) as slow_task:
         slow_task.enter_progress_frame(1, text_label.format(asset_data.asset_name))
         if slow_task.should_cancel():
             break
-        changeZoneMaterials(slow_task, asset_data)
+        change_zone_materials(slow_task, asset_data)
